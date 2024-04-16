1aa3e28ba8b029eb9c18c97023361c1f4db0afef
Ryan2805 <S00237889@atu.ie>
2024-04-16 21:12:17 +0100

    apiservicets file

--- mongodb-express-rest-api/carsales-nosql/src/app/services/car-api.service.ts
+++ mongodb-express-rest-api/carsales-nosql/src/app/services/car-api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { ICar } from '../interfaces/car';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarApiService {
+  private _siteURL = 'http://localhost:5050'; // Adjust the URL to match your API endpoint
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getAllCars(): Observable<ICar[]> {
+    return this._http.get<ICar[]>(`${this._siteURL}/cars`)
+      .pipe(
+        tap(data => console.log('All cars: ', data)),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('An error occurred:', error);
+    return throwError('Something bad happened; please try again later.');
+  }
 }
+
